d271f9bd0aff75134cb2366d6fb75f7eb087c306
Adanieljohnson91 <anthonydanjohnson@gmail.com>
2020-09-03 10:53:41 -0500

    finished SmackTalkingPlayer

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -1,10 +1,25 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A Player who shouts a taunt every time they roll dice
-    public class SmackTalkingPlayer
+    public class SmackTalkingPlayer : Player
     {
-        public string Taunt { get; }
+        private string Taunt { get; }
+
+        public SmackTalkingPlayer(string taunt)
+        {
+            Taunt = taunt;
+        }
+         public override int Roll()
+        {
+            // Return a random number between 1 and DiceSize
+            Console.WriteLine($"{Taunt}");
+            return new Random().Next(DiceSize) + 1;
+        }
+        }
     }
 }
